8615b26bd85bbfec6bdd2624b7ba041d7d7444b4
João Cunha <joaoscunha123@gmail.com>
2024-12-26 12:06:55 -0300

    estruturando  o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contato from './components/pages/Contato';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import NavBar from './components/layout/NavBar';
+import Projects from './components/pages/Projects';
+
 function App() {
   return (
-    <div >
-      <h1>Costs</h1>
-    </div>
+    <Router>
+      <NavBar />
+     
+
+      <Container customClass="min-height">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/newproject" element={<NewProject />} />   
+            <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>  
+      <Footer />
+    </Router>
   );
 }
 
